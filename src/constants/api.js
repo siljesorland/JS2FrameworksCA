a7d4be2d2fa8ffb450d38e8500e7080bb987a4da
Silje Sørland <72661374+siljesorland@users.noreply.github.com>
2022-10-12 13:30:56 +0200

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,4 +2,4 @@
 export const API = "https://thronesapi.com/api/v2/Characters";
 
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const TOKEN_PATH = "/jw/auth/V1/token";
+export const TOKEN_PATH = "/jwt-auth/V1/token";
