9a6960735b689890926e12281a3db7d599aaa737
Silje Sørland <72661374+siljesorland@users.noreply.github.com>
2022-10-12 14:48:55 +0200

    Delete src directory

--- src/components/Paragraph.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Paragraph(props) {
-    return <p>{props.children}</p>;
-}
-
-export default Paragraph;
--- src/constants/api.js
+++
@@ -1,5 +0,0 @@
-
-export const API = "https://thronesapi.com/api/v2/Characters";
-
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const TOKEN_PATH = "/jwt-auth/V1/token";
